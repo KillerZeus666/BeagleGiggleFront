09a45242a96f7d03ff9ad7b4f470063df3065965
KillerZeus666 <katheryng2017@gmail.com>
2025-03-23 15:14:50 -0500

    Agregando ciclo de vida de componente

--- src/app/mascota/mascota-detail/mascota-detail.component.ts
+++ src/app/mascota/mascota-detail/mascota-detail.component.ts
@@ -10,4 +10,18 @@ export class MascotaDetailComponent {
   //Hacer que llegue la Mascota desde componente padre al
   selectedMascota: Mascota | null = null; // Debe estar definida
   @Input() mascota!:Mascota; 
+
+
+  //Inyectas dependencias
+  constructor(){}
+  
+  //Se llama una unica vez cuando el componente se renderiza en Pantalla
+  ngOnInit():void{
+    console.log("ngOnInit de detail"); 
+    //Llamar una api
+  }
+  ngOnChanges():void{
+    console.log("ngOnChanges de detail"); 
+  }
+
 }
