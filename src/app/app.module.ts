8bc87be11cd08053ac4709b8fe92a581b9917350
KillerZeus666 <katheryng2017@gmail.com>
2025-03-23 12:17:46 -0500

    Agregando componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './veterinaria/header/header.component';
+import { MascotaTableComponent } from './mascota/mascota-table/mascota-table.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    MascotaTableComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/mascota/mascota-table/mascota-table.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mascota-table',
+  templateUrl: './mascota-table.component.html',
+  styleUrls: ['./mascota-table.component.css']
+})
+export class MascotaTableComponent {
+
+}
