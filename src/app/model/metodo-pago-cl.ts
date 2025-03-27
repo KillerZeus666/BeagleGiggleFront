ca96d98a221bf2c48b3bf8feaaffab55841f5eaa
Katheryn Guasca <katheryng2017@gmail.com>
2025-03-27 00:45:36 +0000

    Agregando Clases

--- src/app/model/metodo-pago-cl.ts
+++ src/app/model/metodo-pago-cl.ts
@@ -1,2 +1,13 @@
+import { FacturaCL } from "./factura-cl";
+
 export class MetodoPagoCL {
+  id: number;
+  nombre: string;
+  facturas: FacturaCL[];
+
+  constructor(id: number = 0, nombre: string = "", facturas: FacturaCL[] = []) {
+    this.id = id;
+    this.nombre = nombre;
+    this.facturas = facturas;
+  }
 }
--- src/app/model/servicio-cl.ts
+++ src/app/model/servicio-cl.ts
@@ -1,2 +1,31 @@
+import { TestimonioCL } from './testimonio-cl';
+import { TratamientoCL } from './tratamiento-cl';
+import { FacturaCL } from './factura-cl';
+
 export class ServicioCL {
+  idServicio: number;
+  nombre: string;
+  descripcion: string;
+  precioBase: number;
+  testimonios: TestimonioCL[];
+  tratamientos: TratamientoCL[];
+  facturas: FacturaCL[];
+
+  constructor(
+    idServicio: number = 0,
+    nombre: string = '',
+    descripcion: string = '',
+    precioBase: number = 0.0,
+    testimonios: TestimonioCL[] = [],
+    tratamientos: TratamientoCL[] = [],
+    facturas: FacturaCL[] = []
+  ) {
+    this.idServicio = idServicio;
+    this.nombre = nombre;
+    this.descripcion = descripcion;
+    this.precioBase = precioBase;
+    this.testimonios = testimonios;
+    this.tratamientos = tratamientos;
+    this.facturas = facturas;
+  }
 }
