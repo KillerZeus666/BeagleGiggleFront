58f3fa9410f38b7fe7281388afd932a3b1c8280d
Katheryn Guasca <katheryng2017@gmail.com>
2025-03-27 00:51:21 +0000

    Agregando Clase Veterinario

--- src/app/model/tratamiento-cl.ts
+++ src/app/model/tratamiento-cl.ts
@@ -1,2 +1,46 @@
+import { VeterinarioCL } from "./veterinario-cl";
+import { MascotaCL } from "./mascota-cl";
+import { ServicioCL } from "./servicio-cl";
+import { TratamientoMedicamentoCL } from "./tratamiento-medicamento-cl";
+
 export class TratamientoCL {
+    public idTratamiento: number;
+    public codigo: string;
+    public fecha: Date;
+    public detalles: string;
+    public veterinario?: VeterinarioCL;
+    public mascota?: MascotaCL;
+    public servicio?: ServicioCL;
+    public tratamientoMedicamentos: TratamientoMedicamentoCL[];
+
+    constructor();
+    constructor(
+        idTratamiento: number,
+        codigo: string,
+        fecha: Date,
+        detalles: string,
+        veterinario: VeterinarioCL,
+        mascota: MascotaCL,
+        servicio: ServicioCL,
+        tratamientoMedicamentos: TratamientoMedicamentoCL[]
+    );
+    constructor(
+        idTratamiento?: number,
+        codigo?: string,
+        fecha?: Date,
+        detalles?: string,
+        veterinario?: VeterinarioCL,
+        mascota?: MascotaCL,
+        servicio?: ServicioCL,
+        tratamientoMedicamentos?: TratamientoMedicamentoCL[]
+    ) {
+        this.idTratamiento = idTratamiento || 0;
+        this.codigo = codigo || "";
+        this.fecha = fecha || new Date();
+        this.detalles = detalles || "";
+        this.veterinario = veterinario;
+        this.mascota = mascota;
+        this.servicio = servicio;
+        this.tratamientoMedicamentos = tratamientoMedicamentos || [];
+    }
 }
--- src/app/model/veterinario-cl.ts
+++ src/app/model/veterinario-cl.ts
@@ -1,2 +1,42 @@
+import { CitaCL } from "./cita-cl";
+import { TratamientoCL } from "./tratamiento-cl";
+
 export class VeterinarioCL {
+  idVeterinario: number;
+  nombre: string;
+  cedula: string;
+  especialidad: string;
+  foto: string;
+  estado: number;
+  numeroAtenciones: number;
+  nombreUsuario: string;
+  contrasena: string;
+  citas: CitaCL[];
+  tratamientos: TratamientoCL[];
+
+  constructor(
+    idVeterinario: number = 0,
+    nombre: string = "",
+    cedula: string = "",
+    especialidad: string = "",
+    foto: string = "",
+    estado: number = 1,
+    numeroAtenciones: number = 0,
+    nombreUsuario: string = "",
+    contrasena: string = "",
+    citas: CitaCL[] = [],
+    tratamientos: TratamientoCL[] = []
+  ) {
+    this.idVeterinario = idVeterinario;
+    this.nombre = nombre;
+    this.cedula = cedula;
+    this.especialidad = especialidad;
+    this.foto = foto;
+    this.estado = estado;
+    this.numeroAtenciones = numeroAtenciones;
+    this.nombreUsuario = nombreUsuario;
+    this.contrasena = contrasena;
+    this.citas = citas;
+    this.tratamientos = tratamientos;
+  }
 }
