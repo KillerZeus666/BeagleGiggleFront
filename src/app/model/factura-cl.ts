305dfc1c19d3d53ff236f44c3003b07fd8151069
Katheryn Guasca <katheryng2017@gmail.com>
2025-03-27 00:43:26 +0000

    Agregando clases configuradas

--- src/app/model/factura-cl.ts
+++ src/app/model/factura-cl.ts
@@ -1,2 +1,24 @@
+import { MetodoPagoCL } from "./metodo-pago-cl";
+import { ServicioCL } from "./servicio-cl";
+
 export class FacturaCL {
+  idFactura: number;
+  fecha: Date;
+  total: number;
+  metodosPago: MetodoPagoCL[];
+  servicios: ServicioCL[];
+
+  constructor(
+    idFactura: number = 0,
+    fecha: Date = new Date(),
+    total: number = 0.0,
+    metodosPago: MetodoPagoCL[] = [],
+    servicios: ServicioCL[] = []
+  ) {
+    this.idFactura = idFactura;
+    this.fecha = fecha;
+    this.total = total;
+    this.metodosPago = metodosPago;
+    this.servicios = servicios;
+  }
 }
--- src/app/model/medicamento-cl.ts
+++ src/app/model/medicamento-cl.ts
@@ -1,2 +1,39 @@
+import { TratamientoMedicamentoCL } from "./tratamiento-medicamento-cl";
+
 export class MedicamentoCL {
+    public idMedicamento: number;
+    public nombre: string;
+    public precioCompra: number;
+    public precioVenta: number;
+    public unidadesDisponibles: number;
+    public unidadesVendidas: number;
+    public tratamientoMedicamentos: TratamientoMedicamentoCL[];
+
+    constructor();
+    constructor(
+        idMedicamento: number,
+        nombre: string,
+        precioCompra: number,
+        precioVenta: number,
+        unidadesDisponibles: number,
+        unidadesVendidas: number,
+        tratamientoMedicamentos: TratamientoMedicamentoCL[]
+    );
+    constructor(
+        idMedicamento?: number,
+        nombre?: string,
+        precioCompra?: number,
+        precioVenta?: number,
+        unidadesDisponibles?: number,
+        unidadesVendidas?: number,
+        tratamientoMedicamentos?: TratamientoMedicamentoCL[]
+    ) {
+        this.idMedicamento = idMedicamento || 0;
+        this.nombre = nombre || "";
+        this.precioCompra = precioCompra || 0;
+        this.precioVenta = precioVenta || 0;
+        this.unidadesDisponibles = unidadesDisponibles || 0;
+        this.unidadesVendidas = unidadesVendidas || 0;
+        this.tratamientoMedicamentos = tratamientoMedicamentos || [];
+    }
 }
