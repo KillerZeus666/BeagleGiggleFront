115d4e65b7cf983743de8924e4bae918b4987e29
KillerZeus666 <katheryng2017@gmail.com>
2025-04-09 18:58:35 -0500

    Agregando inicio de Sesion, esta el id quemado

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  username = '';
+  password = '';
+  errorMessage = '';
 
+  constructor(private router: Router) {}
+
+  onSubmit() {
+    if (this.username === 'admin' && this.password === '1234') {
+      this.router.navigate(['/admin/dashboard']);
+    } else if (this.username === 'cliente2' && this.password === '1234') {
+      const clienteId = 2; // Este ID debería venir del backend idealmente
+      this.router.navigate(['/mascotas-cliente', clienteId]);
+    } else {
+      this.errorMessage = 'Usuario o contraseña incorrectos.';
+    }
+  }
+  
 }
