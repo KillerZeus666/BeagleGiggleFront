dfd4465065e2cc181568fd28aaa3a5b39df40844
SantiagoP67 <challengeboys399@gmail.com>
2025-04-23 21:39:56 -0500

    servicio medicamento

---
+++ src/app/service/medicamento.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { MedicamentoCL } from '../model/medicamento-cl';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MedicamentoService {
+  private baseUrl = 'http://localhost:8082/cliente';
+  constructor(private http:HttpClient) { }
+
+  findAll():Observable<MedicamentoCL[]>{
+    return this.http.get<MedicamentoCL[]>(`${this.baseUrl}`)
+  }
+
+  getMedicamento(id:number):Observable<MedicamentoCL>{
+    return this.http.get<MedicamentoCL>(`${this.baseUrl}/${id}`)
+  }
+}
