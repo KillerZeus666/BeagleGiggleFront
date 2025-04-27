6dd26ab7d49815778a771ddb75e3ec8c55778ba4
KillerZeus666 <katheryng2017@gmail.com>
2025-04-27 18:57:15 -0500

    Agregando filtro a Medicamentos para que el admin pueda ordenarlso

--- src/app/medicamento/medicamento-table/medicamento-table.component.ts
+++ src/app/medicamento/medicamento-table/medicamento-table.component.ts
@@ -11,6 +11,7 @@ import { MedicamentoService } from 'src/app/service/medicamento.service';
 export class MedicamentoTableComponent implements OnInit {
   medicamentos: MedicamentoCL[] = [];
   nombreMedicamentoBuscar: string = '';
+  opcionOrdenamiento: string = 'id'; // Filtro predeterminado por ID
 
   constructor(
     private medicamentoService: MedicamentoService,
@@ -25,6 +26,7 @@ export class MedicamentoTableComponent implements OnInit {
     this.medicamentoService.findAll().subscribe({
       next: (medicamentos: MedicamentoCL[]) => {
         this.medicamentos = medicamentos;
+        this.ordenarMedicamentos();  // Ordenar al cargar
       },
       error: (err) => {
         console.error('Error al cargar los medicamentos', err);
@@ -40,11 +42,34 @@ export class MedicamentoTableComponent implements OnInit {
       this.medicamentoService.buscarPorNombre(nombre).subscribe({
         next: (resultados: MedicamentoCL[]) => {
           this.medicamentos = resultados;
+          this.ordenarMedicamentos(); // Ordenar los resultados
         },
         error: (err) => {
           console.error('Error al buscar medicamentos', err);
         }
       });
+    }
+  }
+
+  ordenarMedicamentos(): void {
+    switch (this.opcionOrdenamiento) {
+      case 'id':
+        this.medicamentos.sort((a, b) => a.idMedicamento - b.idMedicamento);
+        break;
+      case 'precioCompraAsc':
+        this.medicamentos.sort((a, b) => a.precioCompra - b.precioCompra);
+        break;
+      case 'precioCompraDesc':
+        this.medicamentos.sort((a, b) => b.precioCompra - a.precioCompra);
+        break;
+      case 'precioVentaAsc':
+        this.medicamentos.sort((a, b) => a.precioVenta - b.precioVenta);
+        break;
+      case 'precioVentaDesc':
+        this.medicamentos.sort((a, b) => b.precioVenta - a.precioVenta);
+        break;
+      default:
+        break;
     }
   }
 }
