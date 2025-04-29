7b0b53355c74e71d11cb83eb6d3af8f9ca08025c
Dani2044 <castro-df@javeriana.edu.co>
2025-04-28 20:54:28 -0500

    Estética

--- src/app/cliente/cartas-cliente/cartas-cliente.component.ts
+++ src/app/cliente/cartas-cliente/cartas-cliente.component.ts
@@ -6,5 +6,5 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./cartas-cliente.component.css']
 })
 export class CartasClienteComponent {
-  @Input() cantidadMascotas: number = 0; // <- Recibir cantidad por input
+  @Input() cantidadMascotas: number = 0;
 }
--- src/app/cliente/cliente-pagina-principal/cliente-pagina-principal.component.ts
+++ src/app/cliente/cliente-pagina-principal/cliente-pagina-principal.component.ts
@@ -6,6 +6,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./cliente-pagina-principal.component.css']
 })
 export class ClientePaginaPrincipalComponent {
-  
-  
 }
